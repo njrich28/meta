fe1b571409dcbd4bbf184885116111eefed9e69b
Neil Richardson <njrich28@gmail.com>
2011-11-08 16:56:10 +0000

    Added API module (actually added this time)

---
+++ meta-api/src/main/java/com/github/njrich28/meta/api/App.java
@@ -0,0 +1,13 @@
+package com.github.njrich28.meta.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
